72451add2a8ce71709550afdc9dbb439c5543c34
Isaac Flores <140214233+AisaacS22@users.noreply.github.com>
2023-08-03 15:57:05 -0600

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,7 +1,6 @@
 package com.devmate.parcialr1;
 
 import javafx.application.Application;
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -11,111 +10,109 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Optional;
 
-public class HelloApplication extends Application {
+public class UniversityRegistrationApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
 
-        //Label for name
-        Label nameLabel=new Label("Nombre");
+        //Nombres de los Label
+        Label nameLabel = new Label("Nombre");
+        TextField nameText = new TextField();
 
-        //Text Field for Name
-        TextField nameText=new TextField();
+        // Label de fecha de nacimiento
+        Label dobLabel = new Label("Nacimiento");
+        DatePicker datePicker = new DatePicker();
 
-        //Label for date of birth
-        Label dobLabel=new Label("Nacimiento");
-
-        //date picker to choose date
-        DatePicker datePicker=new DatePicker();
-
-        //Label for gender
-        Label genderLabel=new Label("Género");
-
-        //Toggle group of radio button
-        ToggleGroup groupGender=new ToggleGroup();
-        RadioButton maleRadio=new RadioButton("Hombre");
+        // Label de Genero
+        Label genderLabel = new Label("Género");
+        ToggleGroup groupGender = new ToggleGroup();
+        RadioButton maleRadio = new RadioButton("Hombre");
         maleRadio.setToggleGroup(groupGender);
-        RadioButton femaleRadio=new RadioButton("Mujer");
+        RadioButton femaleRadio = new RadioButton("Mujer");
         femaleRadio.setToggleGroup(groupGender);
 
-        //Label for technologies known
-        Label technologiesLabel=new Label("Tecnologías Conocidas");
+        // Label de los lenguajes de programación
+        Label technologiesLabel = new Label("¿Lenguajes de programación que maneje?");
+        CheckBox javaCheckBox = new CheckBox("Java");
+        CheckBox dotnetCheckBox = new CheckBox("Python");
 
-        //Check box for education
-        CheckBox javaCheckBox=new CheckBox("Java");
-        CheckBox dotnetCheckBox=new CheckBox("Python");
-
-        //Label for education
-        Label educationLabel=new Label("Educación");
-
-        //list view for educational qualification
-        ListView eduList=new ListView();
-        ObservableList<String> data= FXCollections.observableArrayList();
-        data.addAll("Licenciatura","Bachillerato","Maestría","Doctorado");
+        // Label de formación academica
+        Label educationLabel = new Label("Educación/Nivel Academico");
+        ListView<String> eduList = new ListView<>();
+        ObservableList<String> data = FXCollections.observableArrayList();
+        data.addAll("Licenciatura", "Bachillerato", "Maestría", "Doctorado");
         eduList.setItems(data);
-        eduList.setPrefSize(100,100);
+        eduList.setPrefSize(100, 100);
 
-        //Label for Location
-        Label locationLabel=new Label("Localidad");
-
-        //Choice box for location
-        ChoiceBox<String> locationChoiceBox=new ChoiceBox<>();
+        // Label de su localidad
+        Label locationLabel = new Label("Localidad");
+        ChoiceBox<String> locationChoiceBox = new ChoiceBox<>();
         locationChoiceBox.getItems().addAll(
-                "Jutiapa","Quesada","Pajarita","Moyuta","Agua Blanca"
+                "Jutiapa", "Quesada", "Moyuta", "Agua Blanca", "Otro departamento"
         );
 
-        //Label for register
-        Button buttonRegister=new Button("Registrar");
-        Button buttonClear=new Button("Limpiar");
+        Button buttonSelectTime = new Button("Seleccionar Horario");
+        Button buttonRegister = new Button("Registrar");
+        Button buttonClear = new Button("Limpiar");
 
-        //Crating a Grid Pane
-        GridPane gridPane=new GridPane();
+        Label selectedTimeLabel = new Label("Horario seleccionado:");
+        buttonSelectTime.setOnAction(event -> {
+            ObservableList<String> timeOptions = FXCollections.observableArrayList(
+                    "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM"
+            );
 
-        //Setting size for pane
-        gridPane.setMinSize(500,300);
+            ChoiceDialog<String> timeDialog = new ChoiceDialog<>("9:00 AM", timeOptions);
+            timeDialog.setTitle("Seleccionar Horario");
+            timeDialog.setHeaderText("Seleccione un horario de citación:");
+            timeDialog.setContentText("Horario:");
 
-        //Setting the padding
-        gridPane.setPadding(new Insets(10,10,10,10));
+            Optional<String> result = timeDialog.showAndWait();
+            result.ifPresent(selectedTime -> {
+                System.out.println("Horario seleccionado: " + selectedTime);
+                selectedTimeLabel.setText("Horario seleccionado: " + selectedTime); // Actualizar el Label
+            });
+        });
 
-        //Setting the vertical and horizontal gaps between the columns
+        GridPane gridPane = new GridPane();
+        gridPane.setMinSize(500, 300);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
         gridPane.setVgap(5);
         gridPane.setHgap(5);
-
-        //Setting the grid alignment
         gridPane.setAlignment(Pos.CENTER);
 
-        //Arranging all the nodes in the grid
-        gridPane.add(nameLabel,0,0);
-        gridPane.add(nameText,1,0);
+        gridPane.add(nameLabel, 0, 0);
+        gridPane.add(nameText, 1, 0);
 
-        gridPane.add(dobLabel,0,1);
-        gridPane.add(datePicker,1,1);
+        gridPane.add(dobLabel, 0, 1);
+        gridPane.add(datePicker, 1, 1);
 
-        gridPane.add(genderLabel,0,2);
-        gridPane.add(maleRadio,1,2);
-        gridPane.add(femaleRadio,2,2);
+        gridPane.add(genderLabel, 0, 2);
+        gridPane.add(maleRadio, 1, 2);
+        gridPane.add(femaleRadio, 2, 2);
 
-        gridPane.add(technologiesLabel,0,3);
-        gridPane.add(javaCheckBox,1,3);
-        gridPane.add(dotnetCheckBox,2,3);
+        gridPane.add(technologiesLabel, 0, 3);
+        gridPane.add(javaCheckBox, 1, 3);
+        gridPane.add(dotnetCheckBox, 2, 3);
 
-        gridPane.add(educationLabel,0,4);
-        gridPane.add(eduList,1,4);
+        gridPane.add(educationLabel, 0, 4);
+        gridPane.add(eduList, 1, 4);
 
-        gridPane.add(locationLabel,0,5);
-        gridPane.add(locationChoiceBox,1,5);
+        gridPane.add(locationLabel, 0, 5);
+        gridPane.add(locationChoiceBox, 1, 5);
 
-        gridPane.add(buttonClear,0,7);
-        gridPane.add(buttonRegister,1,7);
+        gridPane.add(buttonSelectTime, 0, 6);
+        GridPane.setColumnSpan(buttonSelectTime, 3);
 
-        //Styling nodes
-        buttonRegister.setStyle(
-                "-fx-background-color:darkslateblue; -fx-text-fill: white;"
-        );
-        buttonClear.setStyle(
-                "-fx-background-color:darkred; -fx-text-fill: white;"
-        );
+        gridPane.add(selectedTimeLabel, 1, 6);
+        GridPane.setColumnSpan(selectedTimeLabel, 3);
+
+        gridPane.add(buttonClear, 0, 8);
+        gridPane.add(buttonRegister, 1, 8);
+
+        // Styling nodes
+        buttonRegister.setStyle("-fx-background-color:darkslateblue; -fx-text-fill: white;");
+        buttonClear.setStyle("-fx-background-color:darkred; -fx-text-fill: white;");
         nameLabel.setStyle("-fx-font:normal bold 15px 'serif' ");
         dobLabel.setStyle("-fx-font:normal bold 15px 'serif' ");
         genderLabel.setStyle("-fx-font:normal bold 15px 'serif' ");
@@ -123,10 +120,12 @@ public class HelloApplication extends Application {
         educationLabel.setStyle("-fx-font:normal bold 15px 'serif' ");
         locationLabel.setStyle("-fx-font:normal bold 15px 'serif' ");
 
-        //Setting the background color
-        gridPane.setStyle("-fx-background-color:#9ad3cb;");
+        gridPane.setStyle("-fx-background-color:#ffffff;");
 
-        //Adding action for Clear button
+// estas líneas aseguran que todos los campos y selecciones
+// en el formulario se restablezcan a sus valores iniciales cuando
+// el usuario presiona el botón limpiar
+
         buttonClear.setOnAction(event -> {
             nameText.clear();
             datePicker.setValue(null);
@@ -135,9 +134,10 @@ public class HelloApplication extends Application {
             dotnetCheckBox.setSelected(false);
             eduList.getSelectionModel().clearSelection();
             locationChoiceBox.getSelectionModel().clearSelection();
+            selectedTimeLabel.setText("Horario seleccionado:");
         });
 
-        //Adding action for Register button
+        //Esto imprime las respuestas seleccionadas
         buttonRegister.setOnAction(event -> {
             String nombre = nameText.getText();
             String nacimiento = datePicker.getValue().toString();
@@ -149,10 +149,9 @@ public class HelloApplication extends Application {
             if (dotnetCheckBox.isSelected()) {
                 tecnologias += "Python ";
             }
-            String educacion = (String) eduList.getSelectionModel().getSelectedItem();
+            String educacion = eduList.getSelectionModel().getSelectedItem();
             String localidad = locationChoiceBox.getSelectionModel().getSelectedItem();
 
-            // Aquí podrías realizar alguna acción con los datos, como almacenarlos en una base de datos.
             System.out.println("Registro:\n" +
                     "Nombre: " + nombre + "\n" +
                     "Nacimiento: " + nacimiento + "\n" +
@@ -162,18 +161,10 @@ public class HelloApplication extends Application {
                     "Localidad: " + localidad);
         });
 
-        //Creating a scene object
-        Scene scene=new Scene(gridPane);
-
-        //Setting the title of stage
-        stage.setTitle("Formulario de Registro");
-
-        //Adding scene to the stage
+        Scene scene = new Scene(gridPane);
+        stage.setTitle("Formulario de Inscripción Universitaria");
         stage.setScene(scene);
-
-        //Displaying the contents of the stage
         stage.show();
-
     }
 
     public static void main(String[] args) {
